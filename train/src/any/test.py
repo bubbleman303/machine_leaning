859cfb058a9fe3fe591867948ba6ff9b9dce179c
keizaburo.takashiba@airitech.co.jp <keizaburo.takashiba@airitech.co.jp>
2022-01-07 18:25:24 +0900

    x

--- train/src/any/test.py
+++ train/src/any/test.py
@@ -1,15 +1,19 @@
 from othello.src.othello_agent.board import Board
+from othello.src.othello_agent.agent import NewOthelloAgent
+
 from machine.src.othello.othello_trainer import OthelloTrainer
 import numpy as np
 
+agent = NewOthelloAgent()
 board = Board()
 trainer = OthelloTrainer(load_leaner="othello.pkl")
 learner = trainer.leaner
-for i in range(52):
-
+for i in range(56):
     board.random_put()
 
+his = agent.read_all(board.board, board.turn, board.pss)
 print(board.board)
-print(np.where(board.board == 1)[0].size - np.where(board.board == 2)[0].size)
 x = trainer.predict(board.board)
 print(x)
+score = his[agent.get_flatten_board_str_with_turn(board.board,board.turn)]
+print(score)
